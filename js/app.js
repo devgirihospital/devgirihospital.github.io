c88bb195eaefbcc0f8913dc7b7bad8ce5d4fd6e6
devgirihospital <sablesamarth60@gmail.com>
2025-08-09 17:46:37 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector('.btn');
+
+btn.onclick = () => {
+    window.location.href = "tel:+917498536311";;
+};
+
+
+let btn1 = document.querySelector('.btn1');
+
+btn1.onclick = () => {
+    window.location.href = "tel:+917498536311";;
+};
